16e3c30010b92da0833df1fba314c6179fc3dfba
matcharr <you@example.com>
2020-07-07 23:35:04 +0200

    done

--- exo_10.rb
+++ exo_10.rb
@@ -1,4 +1,10 @@
-puts "Quel age as-tu?"
-user_age = gets.chop
-res = user_age.to_i - 3
-puts res
+n = 1
+user_num = gets.chomp.to_i
+until n > user_num # Jusqu’à ce que n soit plus grand que 10, le code est exécuté.
+  puts "Salut, ça farte ?"
+  n = n + 1
+end 
+
+
+#1.upto(user_num) {|user_num| puts "Salut, ça farte ?"}
+#user_num.times {puts "a"}
---
+++ exo_12.rb
@@ -0,0 +1,2 @@
+user_num = gets.chomp.to_i
+1.upto(user_num) {|user_num| puts user_num}
