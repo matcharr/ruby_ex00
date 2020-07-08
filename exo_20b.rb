724ffa29ed821f825cb1d025276f3b34e1b56b40
kyo <44788022+matcharr@users.noreply.github.com>
2020-07-08 09:13:25 +0200

    Update exo_20b.rb

--- exo_20b.rb
+++ exo_20b.rb
@@ -1,8 +1,16 @@
+#puts "rentre un nombre "
+#nombre = gets.to_i
+#return if nombre < 0 || nombre > 25
+#etage = 1
+#while etage <= nombre
+#  puts '#' * etage
+#  etage += 1
+#  end
+
 puts "rentre un nombre "
 nombre = gets.to_i
 return if nombre < 0 || nombre > 25
-etage = 1
-while etage <= nombre
-  puts '#' * etage
-  etage += 1
-  end
+1.upto(nombre) do |etage|
+  puts " " * (nombre - 1) + "#" * etage
+  nombre -= 1
+end
