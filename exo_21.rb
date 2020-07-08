ed916574c6bf4956ebcf301c57d397ff2c1945ca
kyo <44788022+matcharr@users.noreply.github.com>
2020-07-08 09:14:56 +0200

    Update exo_21.rb

--- exo_21.rb
+++ exo_21.rb
@@ -1,7 +1,8 @@
 puts "rentre un nombre "
 nombre = gets.to_i
 return if nombre < 0 || nombre > 25
-1.upto(nombre) do |etage|
-  puts " " * (nombre - 1) + "#" * etage
-  nombre -= 1
-end
+etage = 1
+while etage <= nombre
+  puts '#' * etage
+  etage += 1
+  end
