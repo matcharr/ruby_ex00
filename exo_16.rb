fb89e973cd8d42a6a4d2a528a1f604fbf6b23e27
kyo <44788022+matcharr@users.noreply.github.com>
2020-09-29 23:55:26 +0200

    Update exo_16.rb

--- exo_16.rb
+++ exo_16.rb
@@ -1,6 +1,7 @@
 puts "rentre ton année de naissance"
 year = gets.to_i
-2020.downto(year).each_with_index do |annee, i|
-    puts "il y a  #{i} ans tu avais #{2020 - year} ans"
+current_year = Time.now.year
+current_year.downto(year).each_with_index do |annee, i|
+    puts "il y a  #{i} ans tu avais #{current_year - year} ans"
     year += 1
 end
